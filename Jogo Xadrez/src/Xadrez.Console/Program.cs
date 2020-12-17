3782df9b2080b848a62bf85e9172c99ed81ff59f
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2020-12-17 15:36:11 -0300

    Melhorando a impresssão do tabuleiro imprimindo a identificação das linhas e colunas, facilitando assim a jogabilidade.

--- Jogo Xadrez/src/Xadrez.Console/Program.cs
+++ Jogo Xadrez/src/Xadrez.Console/Program.cs
@@ -10,20 +10,33 @@ namespace Xadrez.Console
 
             var boardPrint = board.Print();
 
+            System.Console.WriteLine();
+
             PrintBoard(boardPrint);
+
+            System.Console.WriteLine();
         }
 
         static void PrintBoard(Piece[,] board)
         {
+            string columnIdentificationLine = "";
+
             for (int i = 0; i < board.GetLength(0); i++)
             {
+                System.Console.Write($"\t{board.GetLength(0) - i}  ");
+
                 for (int j = 0; j < board.GetLength(1); j++)
                 {
-                    System.Console.Write("- ");
+                    System.Console.Write("-  ");
+
+                    if (i == board.GetLength(0) - 1)
+                        columnIdentificationLine += $"{j + 1}  ";
                 }
 
                 System.Console.WriteLine();
             }
+
+            System.Console.Write($"\t   {columnIdentificationLine}");
         }
     }
 }
