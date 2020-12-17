bb0e50f88b5e097ffaa573393aabca0ca731111b
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2020-12-17 12:13:54 -0300

    Criando projeto de console para permitir jogar no console do Sistema Operacional.

---
+++ Jogo Xadrez/src/jogo-xadrez.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jogo_xadrez.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
