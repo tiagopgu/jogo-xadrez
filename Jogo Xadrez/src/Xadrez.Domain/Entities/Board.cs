e980e78153d16858d78df86fe45287a60368d261
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2020-12-17 14:18:52 -0300

    Implementando classe que representa um tabuleiro.

--- Jogo Xadrez/src/Xadrez.Domain/Entities/Board.cs
+++ Jogo Xadrez/src/Xadrez.Domain/Entities/Board.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Xadrez.Domain.Entities
+﻿namespace Xadrez.Domain.Entities
 {
     public class Board
     {
+        private readonly Piece[,] _pieces;
+
+        public byte AmountLines { get; private set; }
+
+        public byte AmountColumns { get; private set; }
+
+        public Board(byte amountLines, byte amountColumns)
+        {
+            AmountLines = amountLines;
+            AmountColumns = amountColumns;
+
+            _pieces = new Piece[AmountLines, AmountColumns];
+        }
     }
 }
