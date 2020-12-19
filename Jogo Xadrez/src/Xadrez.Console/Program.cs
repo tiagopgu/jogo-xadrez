5a0d88d87fa6d9eec2c34b3b2c22f8c0e988d0ec
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2020-12-19 09:32:30 -0300

    Melhorando o tratamento de erros durante o jogo. Agora, exceções lançadas por causa de uma rega do jogo, não irá parar a aplicação, forçando o jogador realizar a jogada novamente.

--- Jogo Xadrez/src/Xadrez.Console/Program.cs
+++ Jogo Xadrez/src/Xadrez.Console/Program.cs
@@ -17,24 +17,33 @@ namespace Xadrez.Console
 
                 while (game.GameEnded == false)
                 {
-                    System.Console.Write("\tEnter the position of the piece to be moved (ex.: c5): ");
-                    ChessPosition startPosition = Screen.ReadChessPosition();
+                    try
+                    {
+                        System.Console.Write("\tEnter the position of the piece to be moved (ex.: c5): ");
+                        ChessPosition startPosition = Screen.ReadChessPosition();
 
-                    System.Console.Write("\tEnter the final position of the piece (ex.: d8): ");
-                    ChessPosition finalPosition = Screen.ReadChessPosition();
+                        System.Console.Write("\tEnter the final position of the piece (ex.: d8): ");
+                        ChessPosition finalPosition = Screen.ReadChessPosition();
 
-                    game.MovePiece(startPosition, finalPosition);
-                    
+                        game.MovePiece(startPosition, finalPosition);
+                    }
+                    catch (FormatException)
+                    {
+                        System.Console.WriteLine("\n\tError: You entered the position in an invalid format. Press any key to try the move again.");
+                        System.Console.ReadKey();
+                    }
+                    catch (BoardException ex)
+                    {
+                        System.Console.WriteLine($"\n\tError: {ex.Message}. Press any key to try the move again.");
+                        System.Console.ReadKey();
+                    }
+
                     Screen.PrintBoard(game.Board);
                 }
             }
-            catch (BoardException ex)
-            {
-                System.Console.WriteLine("\nError: " + ex.Message);
-            }
             catch (Exception ex)
             {
-                System.Console.WriteLine("\nUnexpected error: " + ex.Message);
+                System.Console.WriteLine("\n\tUnexpected error: " + ex.Message);
             }
         }
     }
