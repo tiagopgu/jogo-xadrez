6eaf16dc1a57f44ef15cca54e6c341375d3f8531
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2021-01-06 17:27:37 -0300

    Separando método para validação inicial da jogada de roque, afim de permitir o uso na validação de outros tipos de roque.

--- Jogo Xadrez/src/Xadrez.Domain/Entities/SpecialMoves.cs
+++ Jogo Xadrez/src/Xadrez.Domain/Entities/SpecialMoves.cs
@@ -6,25 +6,15 @@ namespace Xadrez.Domain.Entities
     {
         public static bool IsSmallCastling(Piece piece, Position destiny, Board board)
         {
-            if (piece == null || piece is King == false || piece.AmountMoviments > 0)
-                return false;
+            Position positionRook = new Position(destiny.Line, (byte)(destiny.Column - 1));
 
-            if (piece.Position.Line != destiny.Line)
+            if (IsCastling(piece, destiny, positionRook, board) == false)
                 return false;
 
             if (destiny.Column - piece.Position.Column != -2)
                 return false;
 
-            Position positionRook = new Position(destiny.Line, (byte)(destiny.Column - 1));
-            Piece rook = board.GetPiece(positionRook);
-
-            if (rook == null || rook is Rook == false || rook.Color != piece.Color || rook.AmountMoviments > 0)
-                return false;
-
             King king = piece as King;
-
-            if (king.IsCheckMovement(king.Position))
-                return false;
 
             Position positionTest = new Position(king.Position.Line, 0);
 
@@ -46,5 +36,30 @@ namespace Xadrez.Domain.Entities
         {
             return IsSmallCastling(piece, destiny, board);
         }
+
+        #region
+
+        private static bool IsCastling(Piece piece, Position destiny, Position positionRook, Board board)
+        {
+            if (piece == null || piece is King == false || piece.AmountMoviments > 0)
+                return false;
+
+            if (piece.Position.Line != destiny.Line)
+                return false;
+
+            King king = piece as King;
+
+            if (king.IsCheckMovement(king.Position))
+                return false;
+
+            Piece rook = board.GetPiece(positionRook);
+
+            if (rook == null || rook is Rook == false || rook.Color != piece.Color || rook.AmountMoviments > 0)
+                return false;
+
+            return true;
+        }
+
+        #endregion
     }
 }
